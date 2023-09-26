be9f43e602fba84aecd1650a062b2a3b864743d7
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2023-09-26 23:33:19 +0800

    add content

--- app.vue
+++ app.vue
@@ -1,5 +1,140 @@
+<script setup lang="ts">
+const title = "Pemerintah Kabupaten Sinjai";
+const description = "Website Resmi Pemerintah Kabupaten Sinjai";
+const url = "https://sinjaikab.go.id/nuxt";
+const image = `${url}/sinjaikab-meta.png`;
+
+useSeoMeta({
+  title: title,
+  ogTitle: title,
+  description: description,
+  ogDescription: description,
+  ogImage: image,
+  twitterCard: "summary_large_image",
+});
+
+const menusLeft = [
+  {
+    name: "pemkab",
+    href: "https://www.sinjaikab.go.id/v4",
+  },
+  {
+    name: "lpse",
+    href: "http://lpse.sinjaikab.go.id/eproc4",
+  },
+  {
+    name: "dprd",
+    href: "http://dprd.sinjaikab.go.id",
+  },
+  {
+    name: "sistik",
+    href: "http://apps.sinjaikab.go.id/sistik",
+  },
+  {
+    name: "kotaku",
+    href: "http://kotaku.pu.go.id",
+  },
+  {
+    name: "rdtr",
+    href: "https://rdtrsinjai.com",
+  },
+  {
+    name: "simpelmi",
+    href: "http://dpmptsp.sinjaikab.go.id",
+  },
+  {
+    name: "asa",
+    href: "https://asa.sinjaikab.go.id",
+  },
+];
+
+const menusRight = [
+  {
+    name: "enikda",
+    href: "http://enikda.sinjaikab.go.id/v3",
+  },
+  {
+    name: "webgis",
+    href: "http://webgis.sinjaikab.go.id",
+  },
+  {
+    name: "sirup",
+    href: "https://sirup.lkpp.go.id/sirup/ro/rekap/klpd/D401",
+  },
+  {
+    name: "simpeg",
+    href: "http://apps.sinjaikab.go.id/simpeg/user/login",
+  },
+  {
+    name: "email",
+    href: "https://sinjaikab.go.id/webmail",
+  },
+  {
+    name: "ppid",
+    href: "http://ppidkab.sinjaikab.go.id",
+  },
+  {
+    name: "jdih",
+    href: "http://jdih.sinjaikab.go.id",
+  },
+  {
+    name: "e-office",
+    href: "http://apps.sinjaikab.go.id/office",
+  },
+];
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <Header /> -->
+
+    <main class="min-h-screen flex flex-col items-center justify-center">
+      <div class="container px-4 mx-auto grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div
+          class="col-span-2 md:col-span-4 md:order-1 flex flex-col justify-center items-center text-center"
+        >
+          <img
+            src="sinjai.png"
+            class="h-20 w-20 mb-4"
+            alt="logo kabupaten sinjai"
+          />
+          <h1 class="text-4xl md:text-6xl font-bold">Selamat Datang</h1>
+          <p class="md:text-lg">Website Resmi Pemerintah Kabupaten Sinjai</p>
+        </div>
+
+        <div
+          class="col-span-2 md:order-2 flex justify-center items-end md:items-center"
+        >
+          <img
+            src="sinjai.png"
+            class="h-40 w-40 md:h-80 md:w-80"
+            alt="logo kabupaten sinjai"
+          />
+        </div>
+
+        <div class="grid grid-cols-2 gap-4 md:order-1">
+          <div v-for="menu in menusLeft" :key="menu.name">
+            <Grid :name="menu.name" :href="menu.href" />
+          </div>
+        </div>
+
+        <div class="grid grid-cols-2 gap-4 md:order-3">
+          <div v-for="menu in menusRight" :key="menu.name">
+            <Grid :name="menu.name" :href="menu.href" />
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <!-- <Footer /> -->
   </div>
 </template>
+
+<style>
+main {
+  background-image: url("bg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
---
+++ components/Grid.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLink :to="href" target="_blank" rel="noreferrer">
+    <div
+      class="h-20 w-20 lg:h-24 lg:w-24 mx-auto group flex items-center justify-center rounded-2xl border shadow lg:hover:rounded-full lg:hover:bg-white lg:hover:border-0"
+    >
+      <NuxtImg
+        :src="`/${name}.png`"
+        :alt="`${name} sinjai`"
+        class="scale-75 lg:scale-90 lg:group-hover:scale-100 transition-all"
+      />
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  href: {
+    type: String,
+    required: true,
+  },
+});
+
+const { name, href } = props;
+</script>
