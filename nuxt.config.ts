b8af1fe01c10c2e5e0cde01204bc6de2f03f6a1f
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2026-09-20 21:28:53 +0800

    perf: optimize font-display swap and prune unused fontawesome styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     },
   },
 
-  css: ["@fortawesome/fontawesome-free/css/all.min.css"],
+  css: ["~/assets/css/fontawesome.css"],
   modules: ["@nuxtjs/tailwindcss", "@vercel/analytics"],
 
   routeRules: {
